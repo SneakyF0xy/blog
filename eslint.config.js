1a444215bad3735b49b7d41fe82db431a6435f6b
SneakyFoxy <d.puiko@yandex.ru>
2025-07-25 15:35:26 +0300

    fix: lint errors, add stylint exclusions

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,10 @@ export default defineConfig([
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: {
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
   },
   tseslint.configs.recommended,
